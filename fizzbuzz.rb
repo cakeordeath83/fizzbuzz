8fee8ab071b5ec0b41be6791f8686a8d943ad040
cakeordeath83 <clair_griffiths@hotmail.com>
2015-12-10 09:08:19 +0000

    Indetation test 2

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,15 +1,15 @@
 class FizzBuzz
-	def fizzbuzz(n)
-		if n % 3 == 0 && n % 5 == 0
-			"fizzbuzz"
-		elsif n % 3 == 0
-			"fizz"
-		elsif n % 5 == 0
-			"buzz"
-		else
-			""
-		end
-	end	
+  def fizzbuzz(n)
+    if n % 3 == 0 && n % 5 == 0
+      "fizzbuzz"
+    elsif n % 3 == 0
+      "fizz"
+    elsif n % 5 == 0
+      "buzz"
+    else
+      ""
+    end
+  end	
 end
 
 
